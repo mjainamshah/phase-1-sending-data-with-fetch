416a54f2d9a4c5db92e5c1a1fa5ce13ed00fdc7e
mjainamshah <jainam.shah@student.moringaschool.com>
2023-10-03 15:32:09 +0300

    COMPLETE ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        "Content-Type" : "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(formData)
+};
+
+function submitData () {
+return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    const newId = object.id;
+    document.body.innerHTML += newId;
+  })
+  .catch(function (error) {
+    document.body.innerHTML += error.message;
+  });
+};
